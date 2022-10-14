30bfd19b4284c79ecad7baa02d6c7c30e52e900b
rebolhero <maria_gabriele28@hotmail.com>
2022-10-13 21:11:57 -0300

    Tela de login. AB#33

--- sojoga/frontend/src/pages/Login/index.jsx
+++ sojoga/frontend/src/pages/Login/index.jsx
@@ -13,27 +13,42 @@ export default function Login() {
 
   return (
     <View style={styles.container}>
-      <TextInput 
-             style={styles.input} 
-             placeholder="E-mail" 
-             maxLength={2} 
-             autoCapitalize="characters" 
-             autoCorrect={false} 
-         /> 
 
-         <TextInput 
-           style={styles.input} 
-           placeholder="Senha"            
-           autoCorrect={false} 
-           secureTextEntry={true} 
-         /> 
+      <ImageBackground source={require('../../assets/images/fundo-login.png')}
+       style={styles.imgFundo}>
+        <Text style={styles.tituloLogin}>Sem amigos para jogar?</Text>
+        <View style={styles.contLogin}>
+        <Text style={styles.titulo}>SEM PROBLEMAS!</Text>
+        <Text style={styles.txtlogin}>Nós temos a solução, aqui você achará seu parceiro ideal para suas gamaplays. Aqui tem pessoas de varios estilos e que jogam games diferentes. </Text>
+        <Text style={styles.txtlogin}>Entre, cadastre-se e conheça nossa rede!</Text>
+        
+        <RectButton style={styles.registrar} onPress={handleNavigationToRegister}> 
+          <Text style={styles.registrarText}>CADASTRAR</Text> 
+        </RectButton> 
+        </View>
+       </ImageBackground>
+      <View style={styles.login}>
+      <Text style={styles.txtPerLogin}>Já possui conta? Então entre abaixo:</Text>
 
-      <RectButton style={styles.button} > 
-              <Text style={styles.buttonText}>ENTRAR</Text> 
-          </RectButton> 
-         <RectButton style={styles.registrar} onPress={handleNavigationToRegister}> 
-              <Text style={styles.registrarText}>CRIAR CONTA</Text> 
-          </RectButton> 
+        <TextInput 
+              style={styles.input} 
+              placeholder="E-mail" 
+              autoCapitalize="characters" 
+              autoCorrect={false} 
+          /> 
+          
+          <TextInput 
+            style={styles.input} 
+            placeholder="Senha"            
+            autoCorrect={false} 
+            secureTextEntry={true} 
+          /> 
+
+        <RectButton style={styles.button} > 
+                <Text style={styles.buttonText}>ENTRAR</Text> 
+            </RectButton> 
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -48,8 +63,9 @@ const styles = StyleSheet.create({
   },
   
   input: { 
-    height: 60, 
-    backgroundColor: '#FFF', 
+    width:320,
+    height: 50, 
+    backgroundColor: '#F5F5F5', 
     border: '1px solid black',
     borderRadius: 10, 
     marginBottom: 8, 
@@ -58,7 +74,8 @@ const styles = StyleSheet.create({
   }, 
 
   button:{
-    height: 60, 
+    width:250,
+    height: 50, 
     backgroundColor: '#3956ff', 
     display:'flex',
     alignItems:'center',
@@ -78,11 +95,76 @@ const styles = StyleSheet.create({
   registrar:{
     display:'flex',
     alignItems:'center',
-    justifyContent:'center'
+    justifyContent:'center',
+    marginTop:20
   },
 
   registrarText:{
+    fontWeight:'bold',
+    color:'#3956FF',
+    fontSize:18,
+    width:250,
+    height:50,
+    backgroundColor:'#FFF',
+    paddingTop:11,
+    textAlign:'center',
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:15,
+  },
+
+  imgFundo: {
+    flex:1,
+    width: '100%',
+    justifyContent:'space-between',
+    alignItems:'center',
+    backgroundColor:'#3956FF',
+    borderBottomLeftRadius:50,
+    borderBottomRightRadius:50,
+  },
+
+  titulo:{
+    color:'#FFF',
+    fontSize:25,
     fontWeight:'bold'
   },
+
+  tituloLogin:{
+    color:'#FFF',
+    fontSize:25,
+    fontWeight:'bold',
+    marginTop:30
+  },
+
+  txtlogin:{
+    color:'#FFF',
+    fontSize:17,
+    textAlign:'justify'
+  },
+
+  contLogin:{
+    display: 'flex',
+    justifyContent:'space-between',
+    flexDirection:'column',
+    height:220,
+    marginBottom:30,
+  },
+
+  txtPerLogin:{
+    fontSize:20,
+    fontWeight:'bold',
+    color:'#3956FF'
+  },
+
+  login:{
+    marginTop:20,
+    height:260,
+    display:'flex',
+    flexDirection:'column',
+    alignItems:'center',
+    justifyContent:'space-between'
+  }
+  
 
 });
