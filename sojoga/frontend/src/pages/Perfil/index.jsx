27452e6d42edaa5203e5f64381204f63b9096f3d
rebolhero <maria_gabriele28@hotmail.com>
2022-10-28 00:58:00 +0000

    Tela Perfil. AB#33

--- sojoga/frontend/src/pages/Perfil/index.jsx
+++ sojoga/frontend/src/pages/Perfil/index.jsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import {View, Image, ImageBackground, StyleSheet, Text, TextInput, Alert} from 'react-native'; 
+import {View, Image, ImageBackground, StyleSheet, Text, TextInput, Alert, ScrollView} from 'react-native'; 
 import {RectButton} from 'react-native-gesture-handler'; 
 import {useNavigation} from '@react-navigation/native'; 
 
@@ -25,9 +25,71 @@ export default function Perfil() {
         </View>
         <View style={styles.bodyPerfil}>
         <Text style={styles.txtTitulo}>Jogos</Text>
-
+        <View style={styles.rolagemJogos}>
+        <ScrollView
+          horizontal={true}
+        >
+          <Image style={styles.imgJogo} source={require('../../assets/images/logo-fortnite.png')}/>
+          <Image style={styles.imgJogo} source={require('../../assets/images/logo-fortnite.png')}/>
+          <Image style={styles.imgJogo} source={require('../../assets/images/logo-fortnite.png')}/>
+          <Image style={styles.imgJogo} source={require('../../assets/images/logo-fortnite.png')}/>
+          <Image style={styles.imgJogo} source={require('../../assets/images/logo-fortnite.png')}/>
+          <Image style={styles.imgJogo} source={require('../../assets/images/logo-fortnite.png')}/>
+        </ScrollView>
         </View>
+        <View style={styles.btnjogo}>
+        <RectButton style={styles.buttonJogo} > 
+            <Text style={styles.buttonText}>Adicionar Jogo</Text> 
+          </RectButton> 
+        </View>
+        <Text style={styles.txtTitulo}>Grupos</Text>
+        <View style={styles.rolagemGrupos}>
+        <ScrollView  style={styles.gp}>
+          <View  style={styles.grupos}>
+              <Image style={styles.imgGrupo} source={require('../../assets/images/gwen.png')}/>
+            <View style={styles.txtGrupos}>
+              <Text style={styles.tituloGrupo}>Os Grandes Construtores</Text>
+              <View style={styles.gpTxt}>
+                <Text style={styles.tituloJogo}>Fortnite</Text>
+                <Text style={styles.tituloJogo}>3/4</Text>
+              </View>
+            </View>
+          </View>
+
+          <View  style={styles.grupos}>
+              <Image style={styles.imgGrupo} source={require('../../assets/images/gwen.png')}/>
+            <View style={styles.txtGrupos}>
+              <Text style={styles.tituloGrupo}>Os Grandes Construtores</Text>
+              <View style={styles.gpTxt}>
+                <Text style={styles.tituloJogo}>Fortnite</Text>
+                <Text style={styles.tituloJogo}>3/4</Text>
+              </View>
+            </View>
+          </View>
+
+          <View  style={styles.grupos}>
+              <Image style={styles.imgGrupo} source={require('../../assets/images/gwen.png')}/>
+            <View style={styles.txtGrupos}>
+              <Text style={styles.tituloGrupo}>Os Grandes Construtores</Text>
+              <View style={styles.gpTxt}>
+                <Text style={styles.tituloJogo}>Fortnite</Text>
+                <Text style={styles.tituloJogo}>3/4</Text>
+              </View>
+            </View>
+          </View>
+        </ScrollView>
+        </View>
+        <View style={styles.btnSair}>
+        <RectButton style={styles.buttonSair}> 
+            <Text style={styles.buttonText}>Sair</Text> 
+        </RectButton> 
+        </View>
+        </View>
+
+        
+        
      </ImageBackground>
+     
     </View>
   );
 }
@@ -92,20 +154,129 @@ const styles = StyleSheet.create({
     color:'#474747',
     fontSize:20,
     fontWeight:'bold',
-    marginTop:20
+    marginTop:5
   },
 
   bodyPerfil:{
     backgroundColor:'#FFF',
     width:'100%',
-    height:590,
+    height:480,
     borderTopEndRadius:40,
     borderTopStartRadius:40,
     display:'flex',
     alignItems:'center',
-    justifyContent:'space-between'
-  }
-
+    justifyContent: 'flex-start'
+  },
   
+  rolagemJogos:{
+    marginTop:10,
+    height:100,
+
+  },
+
+  imgJogo:{
+    
+    width:80,
+    height:80,
+    margin:10
+  },
+
+  buttonJogo:{
+    width:180,
+    height: 40, 
+    backgroundColor: '#3956FF', 
+    display:'flex',
+    alignItems:'center',
+    justifyContent:'center',
+    borderRadius: 10, 
+    marginBottom: 2, 
+    marginTop: 4, 
+    paddingHorizontal: 24, 
+    fontSize: 16, 
+  },
+
+  btnjogo:{
+    display:'flex',
+    justifyContent:'flex-end',
+    flexDirection:'row',
+    width:'90%',
+  },
+
+  rolagemGrupos:{
+    width:'100%',
+    height:130,
+    display:'flex',
+    alignItems:'center'
+  },
+  
+  grupos:{
+    height:90,
+    backgroundColor:'#3956FF',
+    width:'100%',
+    display:'flex',
+    flexDirection:'row',
+    alignItems:'center',
+    borderRadius:20,
+    marginBottom:10
+  },
+
+  gp:{
+    width:'90%',
+    margin:10
+  },
+
+  imgGrupo:{
+    height:70,
+    width:70,
+    borderRadius:70,
+    marginLeft:15,
+    marginRight:15  
+  },
+
+  gpTxt:{
+    display:'flex',
+    width:'60%',
+    flexDirection:'row',
+    justifyContent:'space-between'
+  },
+
+  txtGrupos:{
+    display:'flex',
+    justifyContent:'space-between',
+    height:50
+  },
+
+  tituloGrupo:{
+    fontSize:17,
+    fontWeight:'bold',
+    color:'#FFF'
+  },
+
+  tituloJogo:{
+    fontSize:17,
+    fontWeight:'bold',
+    color:'#FFF'
+  },
+
+  buttonSair:{
+    width:180,
+    height: 45, 
+    backgroundColor: '#FF0000', 
+    display:'flex',
+    alignItems:'center',
+    justifyContent:'center',
+    borderRadius: 10, 
+    marginBottom: 8, 
+    marginTop: 8, 
+    paddingHorizontal: 24, 
+    fontSize: 16, 
+  },
+
+  btnSair:{
+    display:'flex',
+    justifyContent:'center',
+    flexDirection:'row',
+    width:'90%'
+  }
 
 });
